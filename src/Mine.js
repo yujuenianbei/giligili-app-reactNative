ce595e4485e613b8f137f0c196e3e7f8ec23c518
玉珏念北 <365913672@qq.com>
2019-04-16 19:39:16 +0800

    Update Mine.js

--- src/Mine.js
+++ src/Mine.js
@@ -1,24 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
     View,
     Text,
     StyleSheet,
+    WebView,
+    BackHandler, ToastAndroid,
+    Linking,
     TouchableOpacity,
 } from 'react-native';
-
+const dimensions = require('Dimensions')
+const { width } = dimensions.get('window');
 export default class Mine extends Component {
-    static navigationOptions = ({navigation, screenProps}) => ({
+    static navigationOptions = ({ navigation, screenProps }) => ({
         //隐藏顶部导航栏
         header: null,
     });
+    componentWillMount(){
+        BackHandler.addEventListener('hardwareBackPress', this.onBackAndroid);
+}
+    componentWillUnmount() {
+        BackHandler.removeEventListener('hardwareBackPress', this.onBackAndroid);
+    }
+    onBackAndroid = () => {
+        if (this.lastBackPressed && this.lastBackPressed + 2000 >= Date.now()) {
+            //最近2秒内按过back键，可以退出应用。
+            BackHandler.exitApp();
+            return;
+        }
+        this.lastBackPressed = Date.now();
+        ToastAndroid.show('再按一次退出应用',ToastAndroid.SHORT);
+        return true;
+    }
     render() {
         return (
             <View style={styles.container}>
-                <TouchableOpacity style={styles.button} activeOpacity={0.5}>
-                    <Text style={{color: 'white'}}>我的</Text>
-                </TouchableOpacity>
-            </View>
-        );
+                <WebView
+                    ref="webView"
+                    source={{ uri: 'https://www.hpstore.cn' }}
+                    style={{ width: width, height: 500 }}
+                />
+            </View>)
     }
 }
 
@@ -36,5 +57,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#4398ff',
+    },
+    viewForText: {
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 });
